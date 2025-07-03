8461641b54e4adfb76a52ac8a6be2f09f10fb85b
henzinc <henrik@schnellsweb.de>
2025-07-03 22:21:17 +0200

    hotfix exclude root path from middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -12,9 +12,10 @@ export const config = {
          * - _next/static (static files)
          * - _next/image (image optimization files)
          * - favicon.ico (favicon file)
-         * - root path '/'
+         * - root path '/' (excluded to allow public access)
+         * - static assets (images)
          * Feel free to modify this pattern to include more paths.
          */
-        '/((?!_next/static|_next/image|favicon.ico|/|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
+        '/((?!api|_next/static|_next/image|favicon.ico|login|auth|$).*)',
     ],
 }
