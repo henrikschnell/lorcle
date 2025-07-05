88d8ca08ee71290dec99d2f162a569363e180517
henzinc <henrik@schnellsweb.de>
2025-07-05 03:06:46 +0200

    - type fix

--- src/components/CardSelector.tsx
+++ src/components/CardSelector.tsx
@@ -2,12 +2,29 @@
 
 import React from "react";
 
-type Card = {
-    id: string;
+interface Card {
+    id: number;
     number: number;
+    setcode: string;
+    type: string;
+    color: string;
+    cost: number;
+    willpower: number | null;
     fullname: string;
+    name: string;
+    version: string | null;
     fullnamegerman: string;
-};
+    namegerman: string;
+    versiongerman: string | null;
+    flavortext: string;
+    flavortextgerman: string;
+    inkwell: boolean;
+    lore: string | null;
+    rarity: string;
+    raritygerman: string;
+    subtypes: string | null;
+    subtypesgerman: string | null;
+}
 
 type Props = {
     cards: Card[];
