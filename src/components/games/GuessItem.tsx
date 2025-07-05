dedbecc77975cae9d421c071bf2a9458132027d0
henzinc <henrik@schnellsweb.de>
2025-07-05 22:50:24 +0200

    Refactor `GuessRow` and `GuessItem`, add image support for colors, and update card model
    - Enhanced `GuessRow` and `GuessItem` components to support image-based color representation.
    - Added new ink color images and integrated them into the guessing logic with `GuessItem`.
    - Updated data model to include `sets` field in `Card` type and adjusted logic in `GuessRow`.
    - Allowed fetching images from external domains in `next.config.ts`.
    - Disabled caching for `getCards` API fetch request.

--- src/components/games/GuessItem.tsx
+++ src/components/games/GuessItem.tsx
@@ -1,13 +1,38 @@
 import React from 'react';
+import Image from 'next/image';
 
 type GuessState = 'correct' | 'wrong' | 'partial';
 
 type Props = {
     value: string | number;
     state: GuessState;
+    type?: string;
 };
 
-export default function GuessItem({ value, state }: Props) {
+export default function GuessItem({ value, state, type }: Props) {
+    if (type && type === 'img') {
+        switch (value) {
+            case 'Amber':
+                value = '/images/inks/amber.png';
+                break;
+            case 'Amethyst':
+                value = '/images/inks/amethyst.png';
+                break;
+            case 'Emerald':
+                value = '/images/inks/emerald.png';
+                break;
+            case 'Ruby':
+                value = '/images/inks/ruby.png';
+                break;
+            case 'Sapphire':
+                value = '/images/inks/sapphire.png';
+                break;
+            case 'Steel':
+                value = '/images/inks/steel.png';
+                break;
+        }
+    }
+
     const getStateClasses = () => {
         switch (state) {
             case 'correct':
@@ -23,7 +48,17 @@ export default function GuessItem({ value, state }: Props) {
 
     return (
         <div className={`rounded-md w-20 aspect-square flex justify-center items-center border-2 border-primary ${getStateClasses()}`}>
-            <span className="text-wrap text-center px-1 text-sm text-white">{value}</span>
+            {type === 'text' ? (
+                <span className="text-wrap text-center px-1 text-sm text-white">{value}</span>
+            ) : type === 'img' ? (
+                <Image
+                    src={`${value}`}
+                    alt="Ink Image"
+                    width={160}
+                    height={160}
+                    className="w-3/4 h-3/4 object-cover"
+                />
+            ) : null}
         </div>
     );
 }
