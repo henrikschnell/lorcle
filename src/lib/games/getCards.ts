cdc7948c79449e12d626d0538905855fb14266f5
henzinc <henrik@schnellsweb.de>
2025-07-05 02:56:40 +0200

    - type fix

--- src/lib/games/getCards.ts
+++ src/lib/games/getCards.ts
@@ -1,28 +1,4 @@
-type Card = {
-    id: number,
-    number: number,
-    setcode: string,
-    type: string,
-    color: string,
-    cost: number,
-    willpower: number,
-    fullname: string,
-    name: string,
-    version: string,
-    fullnamegerman: string,
-    namegerman: string,
-    versiongerman: string,
-    flavortext: string,
-    flavortextgerman: string,
-    inkwell: boolean,
-    lore: number,
-    rarity: string,
-    raritygerman: string,
-    subtypes: string[],
-    subtypesgerman: string,
-}
-
-export async function getAllCards(): Promise<Card[]> {
+export async function getAllCards(): Promise<unknown[]> {
     const CACHE_TTL = 43200;
 
     const res = await fetch(`${process.env.NEXT_PUBLIC_SITE_URL}/api/cards`, {
