98ab19cad32a8772942bc77431a1273400fb93c2
henzinc <henrik@schnellsweb.de>
2026-01-07 19:04:16 +0100

    Maintenance Screen eingebaut

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,26 @@
 import React from "react";
-import GameBoard from "@/components/games/GameBoard";
-import { getCardsFromHistory } from "@/lib/games/getCardsFromHistory";
-import { getAllCards } from "@/lib/games/getCards";
-import { getClassicCount } from "@/lib/games/getStats";
 
 export default async function Home() {
+    /*
     const [relevantCards, allCards, guessCount] = await Promise.all([
         getCardsFromHistory(),
         getAllCards(),
         getClassicCount(),
     ]);
+    */
 
     return (
+        /*
         <div className="flex flex-col justify-center items-center gap-10">
             <GameBoard cards={allCards} todaysCard={relevantCards[1]} yesterdaysCard={relevantCards[0]} guessCount={guessCount}/>
+        </div>
+         */
+        <div className="flex justify-center items-center h-100 text-3xl">
+            <p className="text-accent text-center leading-20">
+                - Under Maintenance -
+                <br/>
+                Sorry for the inconvenience!
+            </p>
         </div>
     );
 }
